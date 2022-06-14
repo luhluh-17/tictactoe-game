f3b7c67e09746ae7628449114a8b8db91a6205b6
luhluh-17 <castro.ralph17@gmail.com>
2022-06-15 03:39:04 +0800

    adds sepearate module for creating html elements

---
+++ js/modules/element.js
@@ -0,0 +1,41 @@
+export const icon = (name) => {
+  const icon = document.createElement('i')
+  icon.classList.add('fa-solid')
+  icon.classList.add(`fa-${name}`)
+  return icon
+}
+
+export const imageText = (src, text) => {
+  const div = document.createElement('div')
+  div.classList.add('flex-col')
+  div.append(image(src))
+  div.append(heading(text, 6))
+  return div
+}
+
+export const unorderedList = (array) => {
+  const ul = document.createElement('ul')
+  const li = document.createElement('li')
+
+  array.forEach((text) => {
+    const p = document.createElement('p')
+    p.textContent = text
+    li.append(p)
+  })
+
+  ul.append(li)
+  return ul
+}
+
+const heading = (text, size = 4) => {
+  const header = document.createElement(`h${size}`)
+  header.textContent = text
+  return header
+}
+
+const image = (src, alt = 'image') => {
+  const img = document.createElement('img')
+  img.src = src
+  img.alt = alt
+  return img
+}
